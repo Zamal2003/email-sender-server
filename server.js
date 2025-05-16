e9becd4dde3ee770ef50173dcc8141990c5d3197
mdaman9939 <mdjamaluddina007@gmail.com>
2025-05-16 22:56:29 +0530

    first commit

--- server.js
+++ server.js
@@ -44,8 +44,9 @@ app.use(helmet()); // Security headers
 // app.use(cors({ origin: '*' }));
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
+  ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim())
   : ['http://localhost:5173'];
+
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -58,6 +59,8 @@ app.use(cors({
   credentials: true
 }));
 
+// Handle preflight requests globally
+app.options('*', cors());
 
 
 // app.use(
